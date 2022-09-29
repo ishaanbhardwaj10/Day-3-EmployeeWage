cd0d6e0cf4699d9b9e3cd5c1181675c30d41ab44
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-09-29 18:29:29 +0530

    changed the 3 local constant variables into global and public

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 {
     class Program
     {
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-            const int IS_FULL_TIME = 2;
-            const int IS_PART_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             int empWage, empHrs;
